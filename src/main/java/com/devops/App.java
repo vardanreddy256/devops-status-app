5f90a9b161ee63f5ca5ed55e70c150614bce8602
Devalapalli Yasovardhan Reddy <142159511+vardanreddy256@users.noreply.github.com>
2026-09-18 12:55:23 +0530

    Fix spelling of 'Success' in application output
    Corrected spelling of 'Success' to 'sucess' in output.

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -16,8 +16,7 @@ public class App {
         System.out.println("Version     : " + version);
         System.out.println("Environment : " + environment);
         System.out.println("Status      : " + status);
-        System.out.println("Application : " + Sucess);
-        System.out.println("Application : " + Sucess);
+        System.out.println("Application : " + sucess);
         System.out.println("     These Changes Are From Local");
         System.out.println("================================");
     }
